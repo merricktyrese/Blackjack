94cd427e51bc94862ea02be5df36ac27cb77dec2
Brandon Bigham <brandonb210@yahoo.com>
2020-10-20 15:06:51 -0500

    Stage1 for Player class

---
+++ Brandon/src/Player.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    public String name;
+    public ArrayList hand;
+    public int chips;
+
+    public Player(String name){
+        this.name = name;
+        this.hand = ;
+        this.chips = 1000;
+    }
+
+    public void Hit{
+        //code to add another card to hand
+    }
+
+    // public ArrayList ViewHand(){}
+
+    @Override public String toString() {
+        return hand.toString();
+    }
+}
